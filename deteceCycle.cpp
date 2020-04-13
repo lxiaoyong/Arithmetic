1572393609c54ba80bc56d8e2c61c3d032c71bc8
lxiaoyong <790217813@qq.com>
2020-04-13 21:36:59 +0800

    环形链表

---
+++ deteceCycle.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Liu on 2020/4/13.
+//
+#include <set>
+using namespace std;
+
+/*
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 nullptr。
+ * */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode * detectCycle(ListNode *head) {
+    set<ListNode*> nodes;
+    ListNode *_h=head;
+    int t=0;    //计数
+    while(_h!=nullptr){
+        nodes.insert(_h);
+        ++t;
+        // nodes存储的指针大小小于t时，说明已存在该指针，存在回路
+        if(nodes.size()<t){
+            return _h;
+        }
+        _h=_h->next;
+    }
+    return nullptr;
+}
