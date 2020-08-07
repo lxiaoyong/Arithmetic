e4891aa4bfc6a4f3f175fbe3a4ae61cd501b4db6
lxiaoyong <790217813@qq.com>
2020-08-07 20:49:07 +0800

    二叉树的最近公共祖先

--- lowestCommonAncestor.cpp
+++ lowestCommonAncestor.cpp
@@ -11,6 +11,7 @@ struct TreeNode {
     TreeNode *right;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
+
 bool own_p_q (queue<TreeNode *>& node,TreeNode* root, TreeNode* p, TreeNode* q){
     if(!node.empty()) return false;
     if(root== nullptr) return false;
@@ -27,3 +28,13 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
     own_p_q(node,root,p,q);
     return node.front();
 }
+/*
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root==nullptr) return nullptr;
+    if(root==p || root==q) return root;
+    TreeNode* left=lowestCommonAncestor(root->left,p,q);
+    TreeNode* right=lowestCommonAncestor(root->right,p,q);
+    if(left!= nullptr && right!= nullptr) return root;
+    if(left == nullptr) return right;
+    return left;
+}*/
