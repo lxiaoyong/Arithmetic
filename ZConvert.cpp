74c613cb27ca380dc00a68df3f7ba8442391fc8b
lxiaoyong <790217813@qq.com>
2020-08-19 21:54:07 +0800

    update

--- ZConvert.cpp
+++ ZConvert.cpp
@@ -8,6 +8,21 @@ using namespace std;
 string convert(string s, int numRows) {
     if(numRows==1) return s;
     vector<string> vs(numRows);
+    int current_row=0;
+    bool direction=false;
+    for(const char& ch:s){
+        vs[current_row]+=ch;
+        if(current_row==0 || current_row==numRows-1)
+            direction=!direction;
+        current_row+=(direction?1:-1);
+    }
+    string result;
+    for(const string& str:vs) result+=str;
+    return result;
+}
+/*string convert(string s, int numRows) {
+    if(numRows==1) return s;
+    vector<string> vs(numRows);
     int n=2*numRows-2;
     for(int i=0;i<numRows;++i){
         for(int j=i; j < s.size(); j+=2){
@@ -17,4 +32,4 @@ string convert(string s, int numRows) {
     string result;
     for( const string &str:vs) result+=str;
     return result;
-}
+}*/
