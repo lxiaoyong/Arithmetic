b34f8c0d4d6a3bf72aa016c22488bd4ee37ab3f6
lxiaoyong <790217813@qq.com>
2020-08-25 16:30:55 +0800

    回文链表

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -8,7 +8,44 @@ struct ListNode {
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
+ListNode* reverse_list(ListNode* head){
+    if(head==nullptr || head->next==nullptr) return head;
+    ListNode* cur=head,* new_head=head->next,*temp=head->next->next;
+    cur->next= nullptr;
+    new_head->next=cur;
+    while(temp!= nullptr){
+        cur=new_head;
+        new_head=temp;
+        temp=temp->next;
+        new_head->next=cur;
+    }
+    return new_head;
+}
 bool isPalindrome(ListNode* head) {
+    if(head==nullptr || head->next==nullptr) return true;
+    ListNode* slow=head,*fast=head,*pre_slow= nullptr;
+    while(fast!=nullptr && fast->next!= nullptr){
+        fast=fast->next->next;
+        pre_slow=slow;
+        slow=slow->next;
+    }
+    bool palindrome=true;
+    ListNode* reverse_list_head=reverse_list(slow);
+    ListNode* temp=reverse_list_head;
+    fast=head;pre_slow->next= nullptr;
+    while(fast!=nullptr){
+        if(fast->val==temp->val){
+            fast=fast->next;temp=temp->next;
+        }else{
+            palindrome=false;
+            break;
+        }
+    }
+    reverse_list_head=reverse_list(reverse_list_head);
+    pre_slow->next=reverse_list_head;
+    return palindrome;
+}
+/*bool isPalindrome(ListNode* head) {
     ListNode *pl=head;
     vector<int> nums;
     while(pl!=NULL){
@@ -23,5 +60,5 @@ bool isPalindrome(ListNode* head) {
         }
     }
     return flag;
-}
+}*/
 
