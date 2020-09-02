787f5537c71f925924a2196d7cfdead565862c71
lxiaoyong <790217813@qq.com>
2020-09-02 17:23:44 +0800

    无重复字符的最长子串

--- lengthOfLongestSubstring.cpp
+++ lengthOfLongestSubstring.cpp
@@ -3,10 +3,31 @@
 //
 
 //题目：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
-
 #include<string>
-#include<unordered_set>
+#include <unordered_map>
 using namespace std;
+int length_of_longest_substring(string s) {
+    int max_length=1,cur_length=0;
+    unordered_map<char,int> unmap_char_int;
+    for(int i=0;i<s.size();++i){
+        if(unmap_char_int.find(s[i]) == unmap_char_int.end()){
+            ++cur_length;
+            unmap_char_int.insert(make_pair(s[i], i));
+            continue;
+        }else{
+            max_length=max(max_length,cur_length);
+            int index=unmap_char_int[s[i]];
+            cur_length=(index<i-cur_length ? cur_length+1 :min(cur_length,i-index));
+            unmap_char_int[s[i]]=i;
+        }
+    }
+    max_length=max(max_length,cur_length);
+    return max_length;
+}
+// 第一版
+
+#include<unordered_set>
+
 int lengthOfLongestSubstring(string s) {
     unordered_set<char> uSetCh;
     int maxLen=0,currentLen=0;
